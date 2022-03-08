f0bbbb0bac23a8e4d46f86d66e5f3ffe3b702c64
bormannb <99260828+bormannb@users.noreply.github.com>
2022-03-08 15:15:20 +0100

    Add files via upload

---
+++ Eagle.java
@@ -0,0 +1,70 @@
+public class Eagle extends Bird implements Fly {
+
+    private boolean flying;
+    private int altitude;
+
+    public Eagle(String name) {
+        super(name);
+        this.flying = false;
+        this.altitude = 0;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public boolean isFlying() {
+        return flying;
+    }
+
+    @Override
+    public String sing() {
+        return "Screech!";
+    }
+
+// takeOff
+@Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+
+// glide
+@Override
+    public void glide() {
+        if (this.flying && this.altitude > 0) {
+           System.out.printf("%s glides into the air.%n", this.getName());
+        // ohne Name !
+        //    System.out.printf("%s glides into the air.%n", "");
+        }
+    }
+
+// land
+@Override
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't land.%n", this.getName());
+        }
+    }
+
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 325);
+            System.out.printf("%s flies upward, altitude: %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 0);
+            System.out.printf("%s flies downward, altitude: %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+}
